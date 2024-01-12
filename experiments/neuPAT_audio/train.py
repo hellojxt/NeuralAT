a863d6fd2924adbe78128a926c706fe919ea2982
Xutong Jin <jinxutong@pku.edu.cn>
2024-01-12 10:27:45 +0800

    reduce memory use in neuPAT_audio train

--- experiments/neuPAT_audio/train.py
+++ experiments/neuPAT_audio/train.py
@@ -16,15 +16,15 @@ xs = []
 ys = []
 for data_points in data_points_lst:
     data = torch.load(data_points)
-    xs.append(data["x"])
-    ys.append(data["y"].unsqueeze(-1))
+    # remove channel 0 and 2 in "x"
+    x = data["x"][:, :, [1, 3, 4, 5, 6]].reshape(-1, 5)
+    y = data["y"].reshape(-1, 1)
+    xs.append(x)
+    ys.append(y)
 
-xs = torch.cat(xs, dim=0).reshape(-1, xs[0].shape[-1])
-ys = torch.cat(ys, dim=0).reshape(-1, ys[0].shape[-1])
+xs = torch.cat(xs, dim=0)
+ys = torch.cat(ys, dim=0)
 ys = ((ys + 10e-6) / 10e-6).log10()
-
-xs = xs.cuda()
-ys = ys.cuda()
 
 xs_train = xs[: int(len(xs) * 0.8)]
 ys_train = ys[: int(len(ys) * 0.8)]
@@ -54,22 +54,16 @@ scheduler = torch.optim.lr_scheduler.StepLR(
 )
 
 
-# Custom shuffle function that operates on the GPU
-def shuffle_tensors(x, y):
-    indices = torch.randperm(x.size(0)).cuda()
-    return x[indices], y[indices]
-
-
 max_epochs = train_params.get("max_epochs")
 test_step = train_params.get("test_step")
 for epoch_idx in tqdm(range(max_epochs)):
     # Create batches manually
     if epoch_idx % 10 == 0:
-        xs_train, ys_train = shuffle_tensors(xs_train, ys_train)
+        indices = torch.randperm(xs_train.size(0))
 
     for batch_idx in range(0, len(xs_train), batch_size):
-        x_batch = xs_train[batch_idx : batch_idx + batch_size]
-        y_batch = ys_train[batch_idx : batch_idx + batch_size]
+        x_batch = xs_train[indices[batch_idx : batch_idx + batch_size]].cuda()
+        y_batch = ys_train[indices[batch_idx : batch_idx + batch_size]].cuda()
 
         # Forward and backward passes
         y_pred = model(x_batch)
@@ -82,8 +76,8 @@ for epoch_idx in tqdm(range(max_epochs)):
     if epoch_idx % test_step == 0:
         loss_test = []
         for batch_idx in range(0, len(xs_test), batch_size):
-            x_batch = xs_test[batch_idx : batch_idx + batch_size]
-            y_batch = ys_test[batch_idx : batch_idx + batch_size]
+            x_batch = xs_test[indices[batch_idx : batch_idx + batch_size]].cuda()
+            y_batch = ys_test[indices[batch_idx : batch_idx + batch_size]].cuda()
             y_pred = model(x_batch)
             loss = torch.nn.functional.mse_loss(y_pred, y_batch)
             loss_test.append(loss.item())

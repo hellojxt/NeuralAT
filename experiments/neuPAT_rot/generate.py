13e14ee595f7437dda75fbc27f3cb0b53bf4fa69
Xutong Jin <jinxutong@pku.edu.cn>
2024-01-15 23:50:45 +0800

    add roation neuPAT experiments

--- experiments/neuPAT_rot/generate.py
+++ experiments/neuPAT_rot/generate.py
@@ -21,167 +21,117 @@ import meshio
 import torch
 from src.ffat_solve import monte_carlo_solve, bem_solve
 
-data_dir = "dataset/NeuPAT/bowl"
+data_dir = sys.argv[1]
 
 import json
 import numpy as np
 
 
-def sample_uniform_quaternion():
-    """
-    Sample a single uniformly distributed quaternion.
-    """
-    # Uniformly distributed values in [0, 1]
-    u1, u2, u3 = torch.rand(3, device="cuda")
-
-    # Spherical to Cartesian coordinates conversion for uniform 3D distribution
-    sqrt1_u1 = torch.sqrt(1 - u1)
-    sqrt_u1 = torch.sqrt(u1)
-    theta1 = 2 * torch.pi * u2
-    phi = torch.acos(2 * u3 - 1)
-
-    # Cartesian coordinates on a 3D sphere
-    x = sqrt1_u1 * torch.sin(theta1)
-    y = sqrt1_u1 * torch.cos(theta1)
-    z = sqrt_u1 * torch.sin(phi)
-    w = sqrt_u1 * torch.cos(phi)
-
-    return torch.tensor([w, x, y, z], device="cuda")
-
-
-def rotate_points(points, quaternion):
-    """
-    Rotate points by a given quaternion.
-    Points and quaternion should be on CUDA.
-    """
-    # Normalize the quaternion
-    q_norm = quaternion / quaternion.norm()
-    q_vec = q_norm[1:]
-    q_scalar = q_norm[0]
-
-    # Extend q_vec to match the dimensions of points
-    q_vec = q_vec.view(1, 3).expand(points.size(0), 3)
-
-    # Quaternion multiplication (q * p * q^*)
-    q_vec_cross_p = torch.cross(q_vec, points, dim=-1)
-    return (
-        points
-        + 2 * q_scalar * q_vec_cross_p
-        + 2 * torch.cross(q_vec, q_vec_cross_p, dim=-1)
-    )
-
-
 with open(f"{data_dir}/config.json", "r") as file:
     config_data = json.load(file)
 
-data = torch.load(f"{data_dir}/modal_data.pt")
-vertices_vib = data["vertices_vib"].cuda()
-triangles_vib = data["triangles_vib"].cuda()
+data = torch.load(f"{data_dir}/data.pt")
+
 vertices_static = data["vertices_static"].cuda()
 triangles_static = data["triangles_static"].cuda()
-neumann_tri = data["neumann_tri"].cuda()
-vertices_bem = data["vertices_bem"].cuda()
-triangles_bem = data["triangles_bem"].cuda()
-neumann_tri_bem = data["neumann_tri_bem"].cuda()
+vertices_rot = data["vertices_rot"].cuda()
+triangles_rot = data["triangles_rot"].cuda()
+triangles = torch.cat([triangles_static, triangles_rot + len(vertices_static)], dim=0)
+neumann_tri = data["neumann_tri"].cuda().reshape(1, -1)
 
-ks = data["ks"].cuda()
-mode_num = len(ks)
-src_pos_min = torch.tensor(
-    config_data.get("solver", {}).get("src_pos_min"), device="cuda", dtype=torch.float32
+rot_pos = torch.tensor(
+    config_data.get("solver", {}).get("rot_pos"), device="cuda", dtype=torch.float32
 )
-src_pos_max = torch.tensor(
-    config_data.get("solver", {}).get("src_pos_max"), device="cuda", dtype=torch.float32
-)
-trg_pos_min = torch.tensor(
-    config_data.get("solver", {}).get("trg_pos_min"), device="cuda", dtype=torch.float32
-)
-trg_pos_max = torch.tensor(
-    config_data.get("solver", {}).get("trg_pos_max"), device="cuda", dtype=torch.float32
-)
-
-print("src_pos_min:", src_pos_min)
-print("src_pos_max:", src_pos_max)
-print("trg_pos_min:", trg_pos_min)
-print("trg_pos_max:", trg_pos_max)
-trg_sample_num = config_data.get("solver", {}).get("trg_sample_num", 1000)
+rot_axis = config_data.get("solver", {}).get("rot_axis")
+rot_max_degree = config_data.get("solver", {}).get("rot_max_degree")
 src_sample_num = config_data.get("solver", {}).get("src_sample_num", 1000)
-print("trg_sample_num:", trg_sample_num)
-print("src_sample_num:", src_sample_num)
 
-x = torch.zeros(src_sample_num, 64 * 32, 10, dtype=torch.float32)
-y = torch.zeros(src_sample_num, 64 * 32, mode_num, dtype=torch.float32)
-xs = torch.linspace(0, 1, 64, device="cuda", dtype=torch.float32)
-ys = torch.linspace(0, 1, 32, device="cuda", dtype=torch.float32)
-gridx, gridy = torch.meshgrid(xs, ys)
+vertices_rot -= rot_pos
 
-check_correct = True
+freq_min = config_data.get("solver", {}).get("freq_min", 100)
+freq_max = config_data.get("solver", {}).get("freq_max", 10000)
+freq_min_log = np.log10(freq_min)
+freq_max_log = np.log10(freq_max)
+print("freq_min:", freq_min)
+print("freq_max:", freq_max)
 
 
 def calculate_ffat_map():
-    r_min = 1.5
-    r_max = 3.0
-    trg_pos = torch.zeros(64, 32, 3, device="cuda", dtype=torch.float32)
-    r_scale = torch.rand(1).cuda()
-    r = (r_scale * (r_max - r_min) + r_min).item()
-    trg_pos[:, :, 0] = r_scale
-    trg_pos[:, :, 1] = gridx
-    trg_pos[:, :, 2] = gridy
-    trg_pos = trg_pos.reshape(-1, 3)
-    trg_points = get_spherical_surface_points(vertices_vib, r)
-    while True:
-        src_rot = sample_uniform_quaternion()
-        vertices_vib_updated = rotate_points(vertices_vib, src_rot)
-        src_pos = torch.rand(3, device="cuda", dtype=torch.float32)
-        displacement = src_pos * (src_pos_max - src_pos_min) + src_pos_min
-        vertices_vib_updated = vertices_vib_updated + displacement
-        if vertices_vib_updated[:, 1].min() > 0.01:
-            trg_points = rotate_points(trg_points, src_rot)
-            trg_points = trg_points + displacement
-            break
-    vertices = torch.cat([vertices_vib_updated, vertices_static], dim=0).cuda()
-    triangles = torch.cat(
-        [triangles_vib, triangles_static + len(vertices_vib)], dim=0
-    ).cuda()
+    return monte_carlo_solve(
+        vertices,
+        triangles,
+        neumann_tri,
+        ks_batch,
+        trg_points,
+        sample_num,
+        plot=False,
+    )
 
+
+import scipy.spatial.transform as transform
+
+
+def rotate_vertices(vs, rot_axis, rot_degree):
+    vs = transform.Rotation.from_euler(rot_axis, rot_degree.item(), degrees=True).apply(
+        vs.cpu().numpy()
+    )
+    return torch.from_numpy(vs).cuda()
+
+
+sample_num = 8000
+x = torch.zeros(src_sample_num, 64 * 32, 5, dtype=torch.float32)
+y = torch.zeros(src_sample_num, 64 * 32, 1, dtype=torch.float32)
+
+xs = torch.linspace(0, 1, 64, device="cuda", dtype=torch.float32)
+ys = torch.linspace(0, 1, 32, device="cuda", dtype=torch.float32)
+gridx, gridy = torch.meshgrid(xs, ys)
+check_correct = False
+
+for i in tqdm(range(src_sample_num)):
     while True:
-        ffat_map, convergence = monte_carlo_solve(
-            vertices, triangles, neumann_tri, ks, trg_points, 6000, plot=False
+        rot_k = torch.rand(1).cuda()
+        vertices_rot_updated = rotate_vertices(
+            vertices_rot, rot_axis, rot_k * rot_max_degree
         )
-        if convergence:
+        vertices_rot_updated += rot_pos
+        vertices = (
+            torch.cat([vertices_static, vertices_rot_updated], dim=0).float().cuda()
+        )
+        r_min = 3
+        r_max = 4
+        r_scale = torch.rand(1).cuda()
+        trg_pos = torch.zeros(64, 32, 3, device="cuda", dtype=torch.float32)
+        r = (r_scale * (r_max - r_min) + r_min).item()
+        trg_pos[:, :, 0] = r_scale
+        trg_pos[:, :, 1] = gridx
+        trg_pos[:, :, 2] = gridy
+        trg_pos = trg_pos.reshape(-1, 3)
+        trg_points = get_spherical_surface_points(vertices_static, r)
+
+        freq_pos = torch.rand(1, device="cuda", dtype=torch.float32)
+        freq_log = freq_pos * (freq_max_log - freq_min_log) + freq_min_log
+        freq = 10**freq_log
+        k = 2 * np.pi * freq / 343.2
+
+        ks_batch = torch.tensor([-k], dtype=torch.float32).cuda()
+        ffat_map, success = calculate_ffat_map()
+        if success:
+            ffat_map = np.abs(ffat_map)
             break
-    ffat_map = torch.from_numpy(np.abs(ffat_map))
+
     if check_correct:
-        vertices = torch.cat([vertices_vib_updated, vertices_bem], dim=0).cuda()
-        triangles = torch.cat(
-            [triangles_vib, triangles_bem + len(vertices_vib)], dim=0
-        ).cuda()
-        print(vertices.shape, triangles.shape, neumann_tri_bem.shape, ks.shape)
-        ffat_map_bem = np.abs(
-            bem_solve(vertices, triangles, neumann_tri_bem, ks, trg_points, plot=False)
-        )
         import matplotlib.pyplot as plt
 
-        for i in range(8):
-            v_min, v_max = np.min(np.abs(ffat_map_bem[i])), np.max(
-                np.abs(ffat_map_bem[i])
-            )
-            plt.subplot(2, 8, i + 1)
-            plt.imshow(np.abs(ffat_map[i]).reshape(64, 32), vmin=v_min, vmax=v_max)
-            plt.colorbar()
-            plt.subplot(2, 8, i + 9)
-            plt.imshow(np.abs(ffat_map_bem[i]).reshape(64, 32), vmin=v_min, vmax=v_max)
-            plt.colorbar()
-        plt.savefig(f"{data_dir}/compare_{idx}.png")
+        CombinedFig().add_mesh(
+            vertices, triangles, neumann_tri[0].abs(), opacity=1.0
+        ).show()
+        plt.imshow(ffat_map.reshape(64, 32))
+        plt.savefig(f"{data_dir}/figs/{i}.png")
         plt.close()
-        CombinedFig().add_mesh(vertices, triangles).add_points(trg_points).show()
-    return ffat_map, src_pos, trg_pos, src_rot
 
+    x[i, :, 0] = rot_k.cpu()
+    x[i, :, 1] = freq_pos.cpu()
+    x[i, :, 2:5] = trg_pos.cpu()
+    y[i] = torch.from_numpy(ffat_map).T
 
-for idx in tqdm(range(src_sample_num)):
-    ffat_map, src_pos, trg_pos, src_rot = calculate_ffat_map()
-    x[idx, :, :3] = src_pos.cpu()
-    x[idx, :, 3:6] = trg_pos.cpu()
-    x[idx, :, 6:10] = src_rot.cpu()
-    y[idx] = ffat_map.T
-
-torch.save({"x": x, "y": y}, f"{data_dir}/data_{sys.argv[1]}.pt")
+torch.save({"x": x, "y": y}, f"{data_dir}/data/{sys.argv[2]}.pt")
--- experiments/neuPAT_rot/preprocess.py
+++ experiments/neuPAT_rot/preprocess.py
@@ -21,93 +21,73 @@ import configparser
 import meshio
 import torch
 
-data_dir = "dataset/NeuPAT/bowl"
+data_dir = sys.argv[1]
 
 import json
 
 with open(f"{data_dir}/config.json", "r") as file:
     config_data = json.load(file)
 
+vibration_objects = config_data.get("vibration_obj", [])
+vib_obj = vibration_objects[0]
+vib_mesh = vib_obj.get("mesh")
+vib_size = vib_obj.get("size")
+vib_position = vib_obj.get("position")
+vib_mesh = MeshObj(f"{data_dir}/{vib_mesh}", scale=vib_size)
+vib_mesh.vertices = vib_mesh.vertices + vib_position
 
-def precompute_sample_points():
-    vibration_objects = config_data.get("vibration_obj", [])
-    for obj in vibration_objects:
-        mesh = obj.get("mesh")
-        size = obj.get("size")
-        material = obj.get("material")
-        mode_num = obj.get("mode_num")
-        obj_vib = ModalSoundObj(f"{data_dir}/{mesh}")
-        obj_vib.normalize(size)
-        obj_vib.modal_analysis(k=mode_num, material=Material(getattr(MatSet, material)))
-        print(obj_vib.get_frequencies())
+static_objects = config_data.get("static_obj", [])
+if len(static_objects) > 0:
+    static_obj = static_objects[0]
+    static_mesh = static_obj.get("mesh")
+    static_size = static_obj.get("size")
+    static_position = static_obj.get("position")
+    static_mesh = MeshObj(f"{data_dir}/{static_mesh}", scale=static_size)
+    static_mesh.vertices = static_mesh.vertices + static_position
 
-    static_objects = config_data.get("static_obj", [])
-    for obj in static_objects:
-        mesh = obj.get("mesh")
-        size = obj.get("size")
-        position = obj.get("position")
-        obj_static = MeshObj(f"{data_dir}/{mesh}", scale=size)
-        mesh_bem = mesh.replace(".obj", "_bem.obj")
-        obj_static_bem = MeshObj(f"{data_dir}/{mesh_bem}", scale=size)
-        obj_static.vertices = obj_static.vertices + position
-        obj_static_bem.vertices = obj_static_bem.vertices + position
+rot_objects = config_data.get("rot_obj", [])
+rot_obj = rot_objects[0]
+rot_mesh = rot_obj.get("mesh")
+rot_size = rot_obj.get("size")
+rot_position = rot_obj.get("position")
+rot_mesh = MeshObj(f"{data_dir}/{rot_mesh}", scale=rot_size)
+rot_mesh.vertices = rot_mesh.vertices + rot_position
 
-    vertices_vib = torch.from_numpy(obj_vib.surf_vertices).cuda().to(torch.float32)
-    triangles_vib = torch.from_numpy(obj_vib.surf_triangles).cuda().to(torch.int32)
-
-    neumann_vib_tri = np.zeros((mode_num, len(triangles_vib)), dtype=np.complex64)
-    wave_number = []
-    for i in range(mode_num):
-        neumann_vib_tri[i] = obj_vib.get_triangle_neumann(i)
-        wave_number.append(obj_vib.get_wave_number(i))
-
-    neumann_vib = torch.from_numpy(neumann_vib_tri).cuda()
-
-    vertices_static = torch.from_numpy(obj_static.vertices).cuda().to(torch.float32)
-    triangles_static = torch.from_numpy(obj_static.triangles).cuda().to(torch.int32)
-
-    neumann_static = torch.zeros(
-        mode_num, len(triangles_static), dtype=torch.complex64
-    ).cuda()
-
-    vertices_bem = torch.from_numpy(obj_static_bem.vertices).cuda().to(torch.float32)
-    triangles_bem = torch.from_numpy(obj_static_bem.triangles).cuda().to(torch.int32)
-
-    neumann_static_bem = torch.zeros(
-        mode_num, len(triangles_bem), dtype=torch.complex64
-    ).cuda()
-    ks = torch.from_numpy(-np.array(wave_number)).to(torch.float32).cuda()
-
-    neumann_tri = torch.cat([neumann_vib, neumann_static], dim=1).cuda()
-    vertices = torch.cat([vertices_vib, vertices_static], dim=0).cuda()
-    triangles = torch.cat(
+if len(static_objects) > 0:
+    vertices_static = torch.from_numpy(static_mesh.vertices).cuda().to(torch.float32)
+    triangles_static = torch.from_numpy(static_mesh.triangles).cuda().to(torch.int32)
+    vertices_vib = torch.from_numpy(vib_mesh.vertices).cuda().to(torch.float32)
+    triangles_vib = torch.from_numpy(vib_mesh.triangles).cuda().to(torch.int32)
+    vertices_static = torch.cat([vertices_vib, vertices_static], dim=0)
+    triangles_static = torch.cat(
         [triangles_vib, triangles_static + len(vertices_vib)], dim=0
-    ).cuda()
-    CombinedFig().add_mesh(vertices, triangles, neumann_tri[0].real, opacity=1.0).show()
-
-    neumann_tri_bem = torch.cat([neumann_vib, neumann_static_bem], dim=1).cuda()
-    vertices = torch.cat([vertices_vib, vertices_bem], dim=0).cuda()
-    triangles = torch.cat(
-        [triangles_vib, triangles_bem + len(vertices_vib)], dim=0
-    ).cuda()
-    CombinedFig().add_mesh(
-        vertices, triangles, neumann_tri_bem[0].real, opacity=1.0
-    ).show()
-
-    torch.save(
-        {
-            "vertices_vib": vertices_vib,
-            "triangles_vib": triangles_vib,
-            "vertices_static": vertices_static,
-            "triangles_static": triangles_static,
-            "neumann_tri": neumann_tri,
-            "ks": ks,
-            "vertices_bem": vertices_bem,
-            "triangles_bem": triangles_bem,
-            "neumann_tri_bem": neumann_tri_bem,
-        },
-        f"{data_dir}/modal_data.pt",
     )
+    neumann_static = torch.zeros(len(triangles_static), dtype=torch.complex64).cuda()
+    neumann_static[: len(triangles_vib)] = 1
+else:
+    vertices_static = torch.from_numpy(vib_mesh.vertices).cuda().to(torch.float32)
+    triangles_static = torch.from_numpy(vib_mesh.triangles).cuda().to(torch.int32)
+    neumann_static = torch.ones(len(triangles_static), dtype=torch.complex64).cuda()
 
+vertices_rot = torch.from_numpy(rot_mesh.vertices).cuda().to(torch.float32)
+triangles_rot = torch.from_numpy(rot_mesh.triangles).cuda().to(torch.int32)
 
-precompute_sample_points()
+neumann_rot = torch.zeros(len(triangles_rot), dtype=torch.complex64).cuda()
+neumann_tri = torch.cat([neumann_static, neumann_rot], dim=0)
+
+vertices = torch.cat([vertices_static, vertices_rot], dim=0)
+triangles = torch.cat([triangles_static, triangles_rot + len(vertices_static)], dim=0)
+
+print(vertices.shape, triangles.shape, neumann_tri.shape)
+CombinedFig().add_mesh(vertices, triangles, neumann_tri.real, opacity=1.0).show()
+
+torch.save(
+    {
+        "vertices_static": vertices_static,
+        "triangles_static": triangles_static,
+        "vertices_rot": vertices_rot,
+        "triangles_rot": triangles_rot,
+        "neumann_tri": neumann_tri,
+    },
+    f"{data_dir}/data.pt",
+)
